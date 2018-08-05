bc48855a07de30ce0015dc4207a056968f70f718
Jakub Syrek <jksyrek@gmail.com>
2018-08-05 21:53:00 +0200

    Add simple service-worker

---
+++ sw.js
@@ -0,0 +1,19 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('airhorner').then(function(cache) {
+            return cache.addAll(['/', '/index.html', '/cv.html']);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
