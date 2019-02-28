ac478e3978492a1d2f8cdb96639bf4074ea4b676
yqbk <jksyrek@gmail.com>
2019-02-28 11:36:43 +0100

    Add weather component

---
+++ weather/precache-manifest.28e84abf78071354681ac0d781f5de35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5346c2795f64f3102fab",
+    "url": "/weather/static/js/runtime~main.5346c279.js"
+  },
+  {
+    "revision": "898dc69419f8ecc784ca",
+    "url": "/weather/static/js/main.898dc694.chunk.js"
+  },
+  {
+    "revision": "067c7a27a12bc53e1584",
+    "url": "/weather/static/js/2.067c7a27.chunk.js"
+  },
+  {
+    "revision": "898dc69419f8ecc784ca",
+    "url": "/weather/static/css/main.7dd072ed.chunk.css"
+  },
+  {
+    "revision": "0bd7698c800586455f4551f9b18b813c",
+    "url": "/weather/index.html"
+  }
+];
