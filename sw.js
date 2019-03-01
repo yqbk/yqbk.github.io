e1bed2a6b0e547f2baafcad73ccd31142166ef2a
yqbk <jksyrek@gmail.com>
2019-03-01 16:16:32 +0100

    Update sw

--- sw.js
+++ sw.js
@@ -1,19 +1,37 @@
-importScripts('/cache-polyfill.js');
+importScripts("/cache-polyfill.js");
 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('airhorner').then(function(cache) {
-            return cache.addAll(['/', '/index.html', '/cv.html']);
-        })
-    );
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open("yqbk").then(function(cache) {
+      return cache.addAll(["/", "/index.html", "/cv.html"]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function(cacheName) {
+            if (CACHE_NAME !== cacheName && cacheName.startsWith("yqbk")) {
+              return caches.delete(cacheName);
+            }
+          })
+          .map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+});
 
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
