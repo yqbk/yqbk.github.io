ba5d7349c1187966251537999b755f9f5e905c2b
yqbk <jksyrek@gmail.com>
2019-03-03 11:03:13 +0100

    Set relative path to stock tracker app

---
+++ stock_tracker/precache-manifest.e5fab4b71b25f94aa304b648afb8d71a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b0e417d8854438c2228",
+    "url": "/stock_tracker/static/js/runtime~main.1b0e417d.js"
+  },
+  {
+    "revision": "6b30edaf2d53ae98317f",
+    "url": "/stock_tracker/static/js/main.6b30edaf.chunk.js"
+  },
+  {
+    "revision": "75c5d9c9f9aefc6b78b2",
+    "url": "/stock_tracker/static/js/2.75c5d9c9.chunk.js"
+  },
+  {
+    "revision": "6b30edaf2d53ae98317f",
+    "url": "/stock_tracker/static/css/main.46905889.chunk.css"
+  },
+  {
+    "revision": "c391fe72683ac9eb7f459d5778747c9e",
+    "url": "/stock_tracker/index.html"
+  }
+];
--- stock_tracker/service-worker.js
+++ stock_tracker/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11d68c56610dc20ed4e1f45591136ab4.js"
+  "/stock_tracker/precache-manifest.e5fab4b71b25f94aa304b648afb8d71a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/stock_tracker/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
